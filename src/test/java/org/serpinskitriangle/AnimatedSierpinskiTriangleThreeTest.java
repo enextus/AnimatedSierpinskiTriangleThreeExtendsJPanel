53a28e7aaab5a9cb5cd9461330477b4e13b1b4b0
Your Name <you@example.com>
2023-04-06 22:38:57 +0200

    3 triangles serpinski 11

--- src/test/java/org/serpinskitriangle/AnimatedSierpinskiTriangleThreeTest.java
+++ src/test/java/org/serpinskitriangle/AnimatedSierpinskiTriangleThreeTest.java
@@ -1,32 +1,37 @@
 package org.serpinskitriangle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.Point;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AnimatedSierpinskiTriangleThreeTest {
+class AnimatedSierpinskiTriangleThreeTest {
 
-    @Test
-    public void testMidpoint() {
-        AnimatedSierpinskiTriangleThree triangle = new AnimatedSierpinskiTriangleThree();
-        Point p1 = new Point(0, 0);
-        Point p2 = new Point(10, 10);
-        Point midpoint = triangle.midpoint(p1, p2);
-        assertEquals(5, midpoint.x);
-        assertEquals(5, midpoint.y);
+    private AnimatedSierpinskiTriangleThree triangle;
+
+    @BeforeEach
+    void setUp() {
+        triangle = new AnimatedSierpinskiTriangleThree();
     }
 
     @Test
-    public void testGetInitialTriangle() {
-        AnimatedSierpinskiTriangleThree triangle = new AnimatedSierpinskiTriangleThree();
+    void getInitialTriangle() {
         Point[] initialTriangle = triangle.getInitialTriangle(600, 600);
         assertNotNull(initialTriangle);
         assertEquals(3, initialTriangle.length);
         assertEquals(new Point(300, 0), initialTriangle[0]);
         assertEquals(new Point(0, 600), initialTriangle[1]);
         assertEquals(new Point(600, 600), initialTriangle[2]);
+    }
+
+    @Test
+    void midpoint() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(10, 10);
+        Point mid = triangle.midpoint(p1, p2);
+        assertNotNull(mid);
+        assertEquals(new Point(5, 5), mid);
     }
 }
